6b996301cfa6043e2dcf9f58b0d52995f1766497
rohvvn <r.sekhar1033@gmail.com>
2025-09-27 16:41:29 -0500

    sched on site

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const schedule = {
+  title: "CS Courses for 2018-2019",
+  courses: {
+    F101: {
+      term: "Fall",
+      number: "101",
+      meets: "MWF 11:00-11:50",
+      title: "Computer Science: Concepts, Philosophy, and Connections",
+    },
+    F110: {
+      term: "Fall",
+      number: "110",
+      meets: "MWF 10:00-10:50",
+      title: "Intro Programming for non-majors",
+    },
+    S313: {
+      term: "Spring",
+      number: "313",
+      meets: "TuTh 15:30-16:50",
+      title: "Tangible Interaction Design and Learning",
+    },
+    S314: {
+      term: "Spring",
+      number: "314",
+      meets: "TuTh 9:30-10:50",
+      title: "Tech & Human Interaction",
+    },
+  },
+};
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => (
+  <div>
+    <h1>{schedule.title}</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Term</th>
+          <th>Number</th>
+          <th>Meets</th>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.entries(schedule.courses).map(([id, course]) => (
+          <tr key={id}>
+            <td>{course.term}</td>
+            <td>{course.number}</td>
+            <td>{course.meets}</td>
+            <td>{course.title}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
